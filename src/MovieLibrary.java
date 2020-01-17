2bc884a9343005c7e8426230180059578abf0ce7
Anna Biga <aniabiga040896@gmail.com>
2020-01-17 22:32:27 +0100

    Improving the quality of the code

--- src/MovieLibrary.java
+++ src/MovieLibrary.java
@@ -12,7 +12,7 @@ public class MovieLibrary {
         System.out.printf("Tytuł: %s\n", movie.title);
         System.out.printf("Opis: %s\n", movie.description);
         System.out.printf("Reżyser: %s\n", movie.director);
-        System.out.printf("Ocena: %f/10\n", movie.rating);
+        System.out.printf("Ocena: %.1f/10\n", movie.rating);
         System.out.printf("Czy otrzymał Oskara: %s\n", movie.oscars);
     }
 }
